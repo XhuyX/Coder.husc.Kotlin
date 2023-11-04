74bdb8e52a0eb8a0ba1c4a81017ccc0e1e02f1f0
XhuyX <115056461+XhuyX@users.noreply.github.com>
2023-11-04 16:59:15 +0900

    Create Số có tổng các ước số lớn hơn chính nó.kt

---
+++ Số có tổng các ước số lớn hơn chính nó.kt
@@ -0,0 +1,20 @@
+import kotlin.math.sqrt
+
+fun main() {
+    var n:Long= readln().toLong()
+    var s:Long=-n
+    for (i in 1..sqrt(n.toDouble()).toLong()){
+        if (n%i==0.toLong()){
+            s+=i
+            if (i!= n/i){
+                s+=n/i
+            }
+        }
+    }
+    if (s>n){
+        print("Yes")
+    }
+    else{
+        print("No")
+    }
+}
